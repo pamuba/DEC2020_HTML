43f14708bb9dca765122e94939cc5d42ea216f85
pamuba <partha.bora24@gmail.com>
2020-12-29 23:29:28 +0530

    17

---
+++ JS/var_let.js
@@ -0,0 +1,31 @@
+//Scope
+
+// function run(){
+//     var foo = "Foo";
+//     var bar = "Bar";
+
+//     console.log(foo, bar)
+   
+//     {
+//         let baz = "Baz";
+//         console.log(baz)
+//     }
+//     // console.log(baz)
+// }
+// run();
+
+
+//Hoisting
+// function run(){
+//     // var foo;//decleration
+
+//     console.log(foo);//undefined
+//     var foo = "Foo";
+//     console.log(foo)//Foo
+// }
+// run();
+
+//redecleration
+// let foo = "hii"
+// let foo = "Hello"
+// console.log(foo)
