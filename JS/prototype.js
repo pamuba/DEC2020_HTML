545c3bca1b187caf914c9d6d2bca49bd3e4bd3c5
pamuba <partha.bora24@gmail.com>
2021-01-05 23:30:14 +0530

    25

---
+++ JS/prototype.js
@@ -0,0 +1,20 @@
+// function Student(){
+//     this.firstName = 'John',
+//     this.lastName = 'Wick'
+// }
+
+// Student.prototype.age = 15;
+
+// var std1 = new Student();
+// // console.log(std1.firstName+" "+std1.lastName)
+// // std1.age = 16
+// console.log(std1.age)
+
+// var std2 = new Student();
+// // console.log(std2.firstName+" "+std2.lastName)
+// console.log(std2.age)
+
+
+// //fn ob prototype
+// //ob ob __proto__
+
