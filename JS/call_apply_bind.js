e1e504253e36ce1794434622b6e93822055ccf9f
pamuba <partha.bora24@gmail.com>
2021-01-06 21:23:05 +0530

    36

--- JS/call_apply_bind.js
+++ JS/call_apply_bind.js
@@ -1,3 +1,20 @@
+// var person = {
+//     firstName : "John",
+//     lastName : "Wick",
+//     showFullName : function(){
+//         return this.firstName+ " " + this.lastName
+//     }
+// };
+
+// var diffPerson = function(dept, id){
+//     console.log(this.showFullName() + ' I am a person');
+//     console.log(this.showFullName()+' DEPT:' +dept+' ID:'+id )
+// }
+
+// var newFn = diffPerson.bind(person)
+// newFn("HR", 123)
+
+//call
 var person = {
     firstName : "John",
     lastName : "Wick",
@@ -11,5 +28,13 @@ var diffPerson = function(dept, id){
     console.log(this.showFullName()+' DEPT:' +dept+' ID:'+id )
 }
 
-var newFn = diffPerson.bind(person)
-newFn("HR", 123)
+// var newFn = diffPerson.bind(person)
+// newFn("HR", 123)
+// 
+// diffPerson.call(person, "HR", 123);
+
+
+diffPerson.apply(person,["HR", 123]);
+
+
+
