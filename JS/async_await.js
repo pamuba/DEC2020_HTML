d9c1e5c4e396e126533070052ad04090587d8d6b
pamuba <partha.bora24@gmail.com>
2021-01-07 21:28:10 +0530

    42

--- JS/async_await.js
+++ JS/async_await.js
@@ -1,12 +1,12 @@
 
-// isString = function(data){
-//     if(Object.prototype.toString.call(data) === '[object String]')
-//         return true;
-//     else
-//         return false
-// }
+// // isString = function(data){
+// //     if(Object.prototype.toString.call(data) === '[object String]')
+// //         return true;
+// //     else
+// //         return false
+// // }
 
-// console.log(isString(3))
+// // console.log(isString(3))
 
 function a(){
     return new Promise(resolve=>{
@@ -20,7 +20,7 @@ function b(){
     return new Promise(resolve=>{
         setTimeout(()=>{
             resolve('Second');
-        }, 3000)
+        }, 1000)
     });
 }
 
@@ -28,23 +28,37 @@ function c(){
     return new Promise(resolve=>{
         setTimeout(()=>{
             resolve('Third');
-        }, 5000)
+        }, 3000)
     });
 }
 
-// async function msg(){
-//     const aa = await a();
-//     const bb = await b();
-//     const cc = await c();
-
-//     console.log(`${aa} ${bb} ${cc}`)
-// }
-
-
 async function msg(){
-    const [aa, bb, cc] = await Promise.all([a(), b(), c()]);
-    
-    console.log(`${aa} ${bb} ${cc}`)
+    const aa = a().then((r)=>r);
+    const bb = b().then((r)=>r);
+    const cc = await c();
+
+    console.log(`${aa}`)
+    console.log(`${bb}`)
+    console.log(`${cc}`)
 }
 
 msg();
+
+// // async function msg(){
+// //     const [aa, bb, cc] = await Promise.all([a(), b(), c()]);
+
+// //     console.log(`${aa} ${bb} ${cc}`)
+// // }
+
+// msg();
+
+// function n(){
+//   setTimeout(()=>console.log("First"), 3000)
+//   console.log("First")
+//   setTimeout(()=>console.log("Second"), 1000)
+//   console.log("Second")
+//   setTimeout(()=>console.log("Third"), 2000)
+//   console.log("Third")
+// }
+
+// n()
