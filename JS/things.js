161c7ed059629115963833cc21c4d8946a237f4d
pamuba <partha.bora24@gmail.com>
2021-01-11 22:38:33 +0530

    93

--- JS/things.js
+++ JS/things.js
@@ -13,6 +13,18 @@
 // module.exports.pi = pi;
 
 
+// module.exports.counter = function(arr){
+//     return `There are ${arr.length} elements in the array.`
+// }
+
+// module.exports.sum = function(x,y){
+//     return `The sum:${x+y}`
+// }
+
+// module.exports.pi = 3.14;
+
+
+
 var counter = function(arr){
     return `There are ${arr.length} elements in the array.`
 }
@@ -23,6 +35,8 @@ var sum = function(x,y){
 
 var pi = 3.14;
 
-module.exports.counter = counter;
-module.exports.sum = sum;
-module.exports.pi = pi;
+module.exports = {
+    counter:counter,
+    sum:sum,
+    pi:pi
+}
