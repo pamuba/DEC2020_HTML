4181e83bd5c24c42a5719eada1a4e1ce2c3e4928
pamuba <partha.bora24@gmail.com>
2021-01-11 22:35:54 +0530

    92

---
+++ JS/things.js
@@ -0,0 +1,28 @@
+// var counter = function(arr){
+//     return `There are ${arr.length} elements in the array.`
+// }
+
+// var sum = function(x,y){
+//     return `The sum:${x+y}`
+// }
+
+// var pi = 3.14;
+
+// module.exports.counter = counter;
+// module.exports.sum = sum;
+// module.exports.pi = pi;
+
+
+var counter = function(arr){
+    return `There are ${arr.length} elements in the array.`
+}
+
+var sum = function(x,y){
+    return `The sum:${x+y}`
+}
+
+var pi = 3.14;
+
+module.exports.counter = counter;
+module.exports.sum = sum;
+module.exports.pi = pi;
---
+++ JS/useThings.js
@@ -0,0 +1,5 @@
+var ce = require('./things')
+
+console.log(ce.counter(["first", "second", "third"]))
+console.log(ce.pi)
+console.log(ce.sum(23, ce.pi))
