8ca1c3418dd82b4232daf4e8977a433ada02d381
pamuba <partha.bora24@gmail.com>
2021-01-12 22:32:14 +0530

    107

--- AngularJS/script1.js
+++ AngularJS/script1.js
@@ -3,3 +3,14 @@ angular
 .controller("Ctrl", function($scope){
     $scope.message = "Test Message"
 })
+.controller("EmpCtrl", function($scope){
+   var employees = [
+       { firstName: "John", lastName:"Wick", dept:"HR", salary:"999999"},
+       { firstName: "John", lastName:"Wick", dept:"HR", salary:"999999"},
+       { firstName: "John", lastName:"Wick", dept:"HR", salary:"999999"},
+       { firstName: "John", lastName:"Wick", dept:"HR", salary:"999999"},
+       { firstName: "John", lastName:"Wick", dept:"HR", salary:"999999"}
+   ];
+
+   $scope.employees = employees;
+})
