851357f993145744727e6f3223d0010888bff0bf
pamuba <partha.bora24@gmail.com>
2021-01-19 20:35:33 +0530

    142

--- AngularJS/serviceDemo.js
+++ AngularJS/serviceDemo.js
@@ -5,14 +5,17 @@ app.controller('emp', ['$scope','calcFactory' , function($scope, calcFactory){
 
     $scope.doSum = function(){
         // $scope.sum = parseInt($scope.a) + parseInt($scope.b);
-        $scope.sum = calcFactory.getSum($scope.a, $scope.b);
+        $scope.sum = calcFactory.getSum($scope.a, $scope.b, function(r){
+            $scope.sum = r;
+        });
     }
 }]);
 
 app.factory('calcFactory', ['$http','$log', function($http, $log){
     var oCalcService = {};
-    oCalcService.getSum = function(a,b){
-        return parseInt(a) + parseInt(b);
+    oCalcService.getSum = function(a,b, cb){
+        var s =  parseInt(a) + parseInt(b);
+        cb(s);
     };
     return oCalcService;
 }])
