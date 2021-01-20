bca1efcb7f0ceafe091354a59b1f94650968499a
pamuba <partha.bora24@gmail.com>
2021-01-20 22:35:16 +0530

    152

---
+++ redisDemo/index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const redis = require('redis')
+
+const PORT = 5000;
+const REDIS_PORT = 6379;
+
+const client = redis.createClient(REDIS_PORT);
+const app = express();
+ 
+
+// Set response
+function setResponse(username, repos){
+    return `<h2>${username} has ${repos} GitHub repos</h2>`
+}
+
+//Make request to Github for data
+async function getRepos(req,res, next){
+    try {
+        console.log("Fetching Data....")
+        const {username} = req.params;
+        const response = await fetch(`https://api.github.com/users/${username}`)
+        const data = await response.json();
+        const repos = data.public_repos;
+        
+        //Set data to redis
+        client.setex(username, 10, repos);
+        res.send(setResponse(username, repos));
+    } catch (error) {
+        console.error(err);
+        res.status(500);
+    }
+}
+
+//Cache middleware
+function cache(req, res, next){
+    const {username} = req.params
+    client.get(username,(err,data) => {
+        if(err) throw err;
+        if(data != null){
+            res.send(setResponse(username,data))
+        }
+        else{
+            next();
+        }
+    })
+}
+app.get('/repos/:username', cache ,getRepos)
+
+app.listen(PORT, ()=>{
+    console.log(`App is lsitening on port ${PORT}`);
+})
