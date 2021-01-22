2b697c167275cb04bf2534bedd7b772661f3b636
pamuba <partha.bora24@gmail.com>
2021-01-22 22:59:32 +0530

    177

--- AngularJSMEAN/public/controllers/controller.js
+++ AngularJSMEAN/public/controllers/controller.js
@@ -1,14 +1,43 @@
 function AppCtrl($scope, $http){
     console.log("Hello from controller"); 
 
-    $http.get('/contactlist').success(function(response){
-        console.log("got the data from the server");
-        $scope.contactlist = response;
-    });
+    var refresh = function(){
+        $http.get('/contactlist').success(function(response){
+            console.log("got the data from the server");
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    }
+
+    refresh();
 
     $scope.addContact = function(){
         console.log($scope.contact);
-        $http.post('/contactlist', $scope.contact);
+        $http.post('/contactlist', $scope.contact).success(function(response){
+            console.log(response)
+            refresh();
+        });
+    };
+
+    $scope.removeContact = function(id){
+        console.log(id)
+        $http.delete('/contactlist/'+id).success(function(response){
+            refresh();
+        });
+    };
+
+    $scope.edit = function(id){
+        console.log(id);
+        $http.get('/contactlist/'+id).success(function(response){
+            $scope.contact = response;
+        });
+    };
+
+    $scope.update = function(){
+        console.log($scope.contact._id);
+        $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+            refresh();
+        })
     }
     
 }
--- AngularJSMEAN/server.js
+++ AngularJSMEAN/server.js
@@ -19,6 +19,37 @@ app.get('/contactlist', function(req, res){
 
 app.post('/contactlist', function(req, res){
     console.log(req.body);
+    db.contactlist.insert(req.body, function(err, doc){
+        res.json(doc);
+    })
+});
+
+app.delete('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.remove({_id:mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    })
+});
+
+app.get('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.findOne({_id:mongojs.ObjectId(id)}, function(err, doc){
+        res.json(doc);
+    })
+});
+
+app.put('/contactlist/:id', function(req, res){
+    var id = req.params.id;
+    console.log(id);
+    console.log(req.body.name);
+    db.contactlist.findAndModify({
+        query:{_id:mongojs.ObjectId(id)},
+        update:{$set:{name:req.body.name, email:req.body.email, number:req.body.number}},
+        new:true}, function(err, doc){
+            res.json(doc);
+    })
 })
 
 app.use(express.static(__dirname+'/public'));
