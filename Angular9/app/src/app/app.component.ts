ef459be88124a4e827795baf3168e5844f5a9c58
pamuba <partha.bora24@gmail.com>
2021-02-04 21:23:44 +0530

    254

--- Angular9/app/src/app/app.component.ts
+++ Angular9/app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { getMaxListeners } from 'process';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,8 @@ export class AppComponent {
   title = 'app';
   public name = "Angular 9";
   public message = "";
+  topics = ['Angular', 'React', 'Vue']
+
+  userModel = new User("John","joh@getMaxListeners.com", 
+  12345678, '', 'morning', true);
 }
